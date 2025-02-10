c23f26f40ae27a2107f362207f720b278612f85f
rd8479 <rdka964@gmail.com>
2025-02-10 22:14:54 +0330

    fix the game

--- memory game/app.js
+++ memory game/app.js
@@ -47,44 +47,45 @@ function render(){
     root.innerHTML=section
 }
 render()
-// const boxes=document.querySelectorAll('.box')
-// const selectBox=[]
-// setTimeout(function (){
-//     for (const i of boxes) {
-//         i.classList.add('hidden')
-//     }
-// },1000)
-// for (const i of boxes) {
-//     i.addEventListener('click',function (event){
-//         if(selectBox.length===0){
-//             event.target.classList.remove('hidden')
-//             selectBox.push(event.target)
-//         }else{
-//             event.target.classList.remove('hidden')
-//             selectBox.push(event.target)
-//             if(selectBox[0].innerHTML===selectBox[1].innerHTML){
-//                 selectBox[0].classList.add('freez')
-//                 selectBox[1].classList.add('freez')
-//                 selectBox.length=0
-//             }else{
-//                 freezAll()
-//                 setTimeout(function (){
-//                     selectBox[0].classList.add('hidden')
-//                     selectBox[1].classList.add('hidden')
-//                     selectBox.length=0
-//                     unfreezAll()
-//                 },1000)
-//             }
-//         }
-//     })
-// }
-// function freezAll(){
-//     for (const i of boxes) {
-//         i.classList.add('freez')
-//     }
-// }
-// function unfreezAll(){
-//     for (const i of boxes) {
-//         i.classList.remove('freez')
-//     }
-// }
+const boxes=document.querySelectorAll('.box')
+const selectBox=[]
+setTimeout(function (){
+    for (const i of boxes) {
+        i.classList.add('hidden')
+    }
+},1000)
+for (const i of boxes) {
+    i.addEventListener('click',function (event){
+        if(selectBox.length===0){
+            event.target.classList.remove('hidden')
+            selectBox.push(event.target)
+        }else{
+            event.target.classList.remove('hidden')
+            selectBox.push(event.target)
+            if(selectBox[0].innerHTML===selectBox[1].innerHTML){
+                selectBox[0].classList.add('freez')
+                selectBox[1].classList.add('freez')
+                console.log(num)
+                selectBox.length=0
+            }else{
+                freezAll()
+                setTimeout(function (){
+                    selectBox[0].classList.add('hidden')
+                    selectBox[1].classList.add('hidden')
+                    selectBox.length=0
+                    unfreezAll()
+                },1000)
+            }
+        }
+    })
+}
+function freezAll(){
+    for (const i of boxes) {
+        i.classList.add('freez')
+    }
+}
+function unfreezAll(){
+    for (const i of boxes) {
+        i.classList.remove('freez')
+    }
+}
