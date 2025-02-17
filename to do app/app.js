ed95d25a8ed2cfd5c8b3dec3ac28ce5a184a3c6a
rd8479 <rdka964@gmail.com>
2025-02-17 19:43:18 +0330

    add first date in calender with js

--- to do app/app.js
+++ to do app/app.js
@@ -1,5 +1,104 @@
 const option={
     markToday:true,
     markHolidays:true,
+    forceFarsiDigits:true
 }
-kamaDatepicker('test',option)
+kamaDatepicker('todo-date',option)
+
+
+
+
+// DOM nodes
+const input = document.getElementById("todo-text");
+const inputDate=document.getElementById("todo-date")
+const todosData = JSON.parse(localStorage.getItem("todos"))
+const todos = todosData || [];
+let editableitemId = null;
+const date=new Date()
+const converter = (text) => text.replace(/[٠-٩۰-۹]/g,a=>a.charCodeAt(0)&15);
+inputDate.value=converter(date.toLocaleDateString("fa-IR"));
+function handleAddTodo() {
+
+    const inputVal = input.value;
+    const dateVal=inputDate.value;
+    if (inputVal&&dateVal) {
+        const newTodo = {
+            id: todos.length > 0 ? todos.at(-1).id + 1 : 1,
+            text: inputVal,
+            isDone: false,
+            date:dateVal
+        }
+
+        todos.push(newTodo)
+        input.value = ""
+        console.log(todos)
+        // renderTodos()
+    }
+}
+
+// function renderTodos() {
+
+//     localStorage.setItem("todos", JSON.stringify(todos));
+
+//     const template = todos.map(item => {
+//         return `
+//         <li id="${item.id}" style="color:red">
+//             <input onchange="handleChangeCheckbox(this,${item.id})" type="checkbox" ${item.isDone ? "checked" : ""} />
+//             ${item.id === editableitemId ? `<input id="editInput" value="${item.title}" />` : `<span>${item.title}</span>`}
+//             <button onclick="deleteItem(${item.id})">delete</button>
+//             ${item.id === editableitemId ? `<button onclick="saveEdit()">save</button>` : `<button onclick="editItem(${item.id})">edit</button>`}
+            
+//         </li>
+//         `
+//     })
+
+//     const temp = template.join("")
+
+//     root.innerHTML = temp
+// }
+
+// renderTodos()
+
+// function handleChangeCheckbox(element, id) {
+//     const foundIndex = todos.findIndex(item => item.id === id);
+
+//     todos[foundIndex].isDone = element.checked;
+//     renderTodos();
+// }
+
+// function saveEdit() {
+//     const editInputValue = document.getElementById("editInput").value;
+
+//     if (editInputValue) {
+//         const foundIndex = todos.findIndex(item => item.id === editableitemId);
+//         todos[foundIndex].title = editInputValue;
+//     }
+
+//     editableitemId = null;
+
+//     renderTodos();
+
+// }
+
+// function editItem(id) {
+//     editableitemId = id;
+//     renderTodos();
+// }
+
+
+// function deleteItem(itemId) {
+//     const foundIndex = todos.findIndex(item => item.id === itemId);
+
+//     todos.splice(foundIndex, 1);
+
+//     renderTodos();
+
+// }
+
+// function handleKeyPress(evt) {
+//     if (evt.key === "Enter") {
+//         handleAddTodo();
+//     }
+// }
+
+// input.addEventListener("keypress", handleKeyPress)
