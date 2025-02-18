462f9b700c8a5a6d5387387292d2c70113464feb
rd8479 <rdka964@gmail.com>
2025-02-18 23:54:19 +0330

    rest when focus on the textarea

--- to do app/app.js
+++ to do app/app.js
@@ -6,7 +6,6 @@ const option={
 kamaDatepicker('todo-date',option)
 
 
-
 const input = document.getElementById("todo-text");
 const inputDate=document.getElementById("todo-date")
 const todosData = JSON.parse(localStorage.getItem("todos"))
@@ -30,82 +29,90 @@ function handleAddTodo() {
         todos.push(newTodo)
         input.value = ""
         
-        // renderTodos()
+        renderTodos()
     }
 }
 function changeVal(evt){
     if(evt.keyCode!==104){
-        if((input.value.length+1)%145===0 &&((input.value.length+1)/145)>2&&((input.value.length+1)/145)<7){
-            input.style.height=`${((input.value.length+1)/145)*3+5}vh`
+        if((input.value.length+1)%141===0 &&((input.value.length+1)/141)>2&&((input.value.length+1)/141)<7){
+            input.style.height=`${((input.value.length+1)/141)*3+5}vh`
+            console.log(((input.value.length+1)/141)*3+5)
         }
     }
     else{
         input.style.height=`${2*3+5}vh`
     }
 }
-// function renderTodos() {
+function erasTextArea(){
+    input.value=""
+    input.style.height='9vh'
+}
+function failPlaceholder(){
+    input.removeAttribute('placeholder')
+}
+function renderTodos() {
 
-//     localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 
-//     const template = todos.map(item => {
-//         return `
-//         <li id="${item.id}" style="color:red">
-//             <input onchange="handleChangeCheckbox(this,${item.id})" type="checkbox" ${item.isDone ? "checked" : ""} />
-//             ${item.id === editableitemId ? `<input id="editInput" value="${item.title}" />` : `<span>${item.title}</span>`}
-//             <button onclick="deleteItem(${item.id})">delete</button>
-//             ${item.id === editableitemId ? `<button onclick="saveEdit()">save</button>` : `<button onclick="editItem(${item.id})">edit</button>`}
+    const template = todos.map(item => {
+        return `
+        <li id="${item.id}" style="color:red">
+            <input onchange="handleChangeCheckbox(this,${item.id})" type="checkbox" ${item.isDone ? "checked" : ""} />
+            ${item.id === editableitemId ? `<input id="editInput" value="${item.title}" />` : `<span>${item.title}</span>`}
+            <button onclick="deleteItem(${item.id})">delete</button>
+            ${item.id === editableitemId ? `<button onclick="saveEdit()">save</button>` : `<button onclick="editItem(${item.id})">edit</button>`}
             
-//         </li>
-//         `
-//     })
+        </li>
+        `
+    })
 
-//     const temp = template.join("")
+    const temp = template.join("")
 
-//     root.innerHTML = temp
-// }
+    root.innerHTML = temp
+}
 
-// renderTodos()
+renderTodos()
 
-// function handleChangeCheckbox(element, id) {
-//     const foundIndex = todos.findIndex(item => item.id === id);
+function handleChangeCheckbox(element, id) {
+    const foundIndex = todos.findIndex(item => item.id === id);
 
-//     todos[foundIndex].isDone = element.checked;
-//     renderTodos();
-// }
+    todos[foundIndex].isDone = element.checked;
+    renderTodos();
+}
 
-// function saveEdit() {
-//     const editInputValue = document.getElementById("editInput").value;
+function saveEdit() {
+    const editInputValue = document.getElementById("editInput").value;
 
-//     if (editInputValue) {
-//         const foundIndex = todos.findIndex(item => item.id === editableitemId);
-//         todos[foundIndex].title = editInputValue;
-//     }
+    if (editInputValue) {
+        const foundIndex = todos.findIndex(item => item.id === editableitemId);
+        todos[foundIndex].title = editInputValue;
+    }
 
-//     editableitemId = null;
+    editableitemId = null;
 
-//     renderTodos();
+    renderTodos();
 
-// }
+}
 
-// function editItem(id) {
-//     editableitemId = id;
-//     renderTodos();
-// }
+function editItem(id) {
+    editableitemId = id;
+    renderTodos();
+}
 
 
-// function deleteItem(itemId) {
-//     const foundIndex = todos.findIndex(item => item.id === itemId);
+function deleteItem(itemId) {
+    const foundIndex = todos.findIndex(item => item.id === itemId);
 
-//     todos.splice(foundIndex, 1);
+    todos.splice(foundIndex, 1);
 
-//     renderTodos();
+    renderTodos();
 
-// }
+}
 
-// function handleKeyPress(evt) {
-//     if (evt.key === "Enter") {
-//         handleAddTodo();
-//     }
-// }
+function handleKeyPress(evt) {
+    if (evt.key === "Enter") {
+        handleAddTodo();
+    }
+}
 
-// input.addEventListener("keypress", handleKeyPress)
+input.addEventListener("keypress", handleKeyPress)
